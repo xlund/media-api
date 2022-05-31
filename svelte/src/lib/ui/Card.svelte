25aba53748326e8fe5a2f2124b539c6b4e425e87
Erik Näslund <contact@eriknaslund.com>
2022-05-31 22:57:45 +0200

    Create single card component

---
+++ svelte/src/lib/ui/Card.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { Image, Video } from 'src/types/types';
+	export let type: string = 'image';
+	export let data: Image | Video;
+	const base = 'http://0.0.0.0:4000/api';
+</script>
+
+<div class="card">
+	<picture>
+		<img
+			class={type == 'image' ? 'image' : 'poster'}
+			src={`${base}/${type == 'image' ? 'images' : 'videos/posters'}/${data.id}`}
+			alt=""
+		/>
+	</picture>
+	<h2 class="title">{data.title}</h2>
+</div>
+
+<style>
+	.title {
+		font-weight: 500;
+	}
+	.image,
+	.poster {
+		height: 200px;
+	}
+	.image {
+		object-fit: cover;
+		width: 100%;
+	}
+</style>
