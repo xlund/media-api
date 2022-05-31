da8758474721330070c9f33155aaa13294b1ff9d
Erik Näslund <contact@eriknaslund.com>
2022-05-31 20:28:11 +0200

    Create VideoCard component

--- svelte/src/routes/videos/index.svelte
+++ svelte/src/routes/videos/index.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
+	import VideoCard from '$lib/video/VideoCard.svelte';
 	import type { Video } from 'src/types/base';
 	export let videos: Video[];
-
-	async function setPlaying(id: number) {
-		const res = await fetch('http://localhost:4000/api/videos/playing/', {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({
-				videoId: id
-			})
-		});
-		const data = await res.text();
-		console.log(data);
-	}
 </script>
 
 <svelte:head>
@@ -25,12 +12,7 @@
 	<h1>Videos</h1>
 	<div class="grid">
 		{#each videos as video (video.id)}
-			<div class="card" on:click={() => setPlaying(video.id)}>
-				<picture>
-					<img class="image" src={`http://0.0.0.0:4000/api/videos/posters/${video.id}`} alt="" />
-				</picture>
-				<h2 class="title">{video.title}</h2>
-			</div>
+			<VideoCard {video} />
 		{/each}
 	</div>
 </div>
@@ -42,16 +24,6 @@
 		margin: var(--column-margin-top) auto 0 auto;
 		line-height: 1;
 	}
-
-	.title {
-		font-weight: 500;
-	}
-
-	.image {
-		height: 200px;
-		width: auto;
-	}
-
 	.grid {
 		display: grid;
 		grid-template-columns: 1fr 1fr 1fr;
