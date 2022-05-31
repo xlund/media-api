fdec215e6ef7246dda93f9307dcfa54aacd008bb
Erik Näslund <contact@eriknaslund.com>
2022-05-31 17:53:06 +0200

    Remove demo code

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -12,9 +12,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-        Welcome to media/api
-	</h1>
+	<h1>Welcome to media/api</h1>
 
 	<Counter />
 </section>
@@ -30,20 +28,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
